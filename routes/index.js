537c738b5100bc497e489a1280ce90e11eaafe12
Nikita Valitov <valitov.nekit@yandex.ru>
2023-10-29 21:16:14 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
+const { ERROR_CODE } = require('../constants/constants');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
+router.use('*', (req, res) => {
+  res
+    .status(ERROR_CODE.NOT_FOUND)
+    .send({ message: 'Страница не найдена.' });
+});
 
 module.exports = router;
