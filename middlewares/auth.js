2aa641892669d0df7dbef41ee04424291b4240b7
Nikita Valitov <valitov.nekit@yandex.ru>
2023-11-12 15:35:02 +0300

    fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 const jwt = require('jsonwebtoken');
 const { ERROR_CODE } = require('../constants/constants');
 
@@ -9,17 +10,13 @@ const handleAuthError = (res) => {
 
 const extractBearerToken = (header) => header.replace('Bearer ', '');
 
-// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
   if (!authorization || !authorization.startsWith('Bearer ')) {
     handleAuthError(res);
   }
-
   const token = extractBearerToken(authorization);
   let payload;
-
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
