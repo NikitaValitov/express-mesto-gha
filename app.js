31e8e858cb9e57b89573fd6f502f831a6a5e9867
Nikita Valitov <valitov.nekit@yandex.ru>
2023-11-29 13:19:37 +0300

    fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { centralizedErrorHandler } = require('./middlewares/centralizedErrorHandler');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(cors());
